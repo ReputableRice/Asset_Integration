486e86609005e002cdb6db6ec0a5264e041e6f85
Skailodon <jackiehtruong@outlook.com>
2024-09-30 22:25:05 -0700

    Fixed Reanimate in Package.json and Styled Sandwhich.tsx

--- app/(tabs)/cheese.tsx
+++ app/(tabs)/cheese.tsx
@@ -4,7 +4,7 @@ import { Link } from "expo-router";
 export default function Page() {
     return (
         <View className="m-2">
-            <Link href="/sandwhich" className="border-red-700 border-2 bg-red-500 flex-1 text-center">Sandwhich</Link>
+        <Link href="/sandwhich" className="rounded-lg bg-cyan-500 flex-1 text-center p-5 text-lg w-1/4 hover:bg-cyan-100 hover:w-1/5 transition-all drop-shadow-lg">Go To Sandwhich</Link>
         </View>
     )
 }
