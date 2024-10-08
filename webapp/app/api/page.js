b30125c94d16a56451076340861129d9f29e8eb3
Skailodon <jackiehtruong@outlook.com>
2024-10-08 15:51:15 -0700

    functional rendering of api content

--- webapp/app/api/page.js
+++ webapp/app/api/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function Page() {
-    const DATA_URL = "https://pokeapi.co/api/v2/pokemon/ditto";
+    const DATA_URL = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=5";
     const [data, setData] = useState(null);
 
     async function fetchData() {
@@ -12,22 +12,31 @@ export default function Page() {
         console.log(response);
         const data = await response.json()
         setData(data)
+
+        //add try catch function here
     }
 
-    const DisplayProducts = () => {
+    const DisplayMedia = () => {
         if (data) {
 
-            const productsList = [];
-            data.forEach((product, index) => {
-                productsList.push(
-                    <li key={index}>{product.name}</li>
+            const mediaList = [];
+            data.forEach((media, index) => {
+
+
+
+                mediaList.push(
+                    <li key={index} className="w-3/4 m-auto">
+                        <img className="rounded-lg m-auto mb-5 mt-10" src={media.url}/>
+                        <h3 className="font-bold">{media.title}</h3>
+                        <p>{media.explanation}</p>
+                    </li>
                 )
             });
             
             return (
-                <div className="border-4 border-black p-4 mb-4">
+                <div className="">
                     <ul >
-                        {productsList}
+                        {mediaList}
                     </ul>
                 </div>
             )
@@ -43,16 +52,16 @@ export default function Page() {
     }
 
     return (
-        <div className="p-4 bg-yellow-300">
-            <header className="border-4 border-black p-4 mb-4">
-                <h1>Welcome to my Product Page</h1>
+        <div className="p-4 w-11/12 m-auto">
+            <header className="border-4 border-black p-4 mb-4 bg-yellow-300">
+                <h1>Welcome to my Images Page</h1>
                 <button
-                    className="border-neutral-200 bg-white bh-black px-6"
+                    className="border-neutral-200 bg-white bh-black px-6 rounded-md"
                 onClick={() => fetchData()}
-                >Fetch Products</button>
+                >Fetch Images</button>
             </header>
             Welcome to my API Page
-            <DisplayProducts />
+            <DisplayMedia />
         </div>
     );
 }
